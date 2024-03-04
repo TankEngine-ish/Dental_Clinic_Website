5b28773a50384d464adcfbb0c8023eeccb4bef06
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2024-03-04 14:20:39 +0200

    add message_name to contact form

--- dental/website/views.py
+++ dental/website/views.py
@@ -5,4 +5,10 @@ def home(request):
 
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == 'POST':
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        return render(request, 'contact.html', {})
