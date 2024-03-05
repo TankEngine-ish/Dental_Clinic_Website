eb707cacd34543fec4dd0d9806dc75d0685a80bd
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2024-03-05 22:48:54 +0200

    fix typo in html at 4.jpg

--- dental/dental/settings.py
+++ dental/dental/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
