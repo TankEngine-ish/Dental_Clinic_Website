3771188fedacfd360af28699b9dd0ab53cf3bce2
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2024-03-05 21:46:48 +0200

    change website path

---
+++ website/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == 'POST':
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        send_mail(
+            message_name, 
+            message, 
+            message_email,
+            ['dripsterg@gmail.com'],
+            fail_silently=False, # if the email is not sent, it will raise an error
+        )
+             
+         
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        return render(request, 'contact.html', {})
