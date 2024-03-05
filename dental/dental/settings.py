f85eee627ffd4bfa1c2129e8cc5a883aa5b86801
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2024-03-06 01:53:09 +0200

    set debug to False

--- dental/dental/settings.py
+++ dental/dental/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0x1xrbn^$5rxedoq+bsxbj^qog%mkk8_vi8myo8ueyjxf$io0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
