f5e0bfadf3852e4193ed24fecea60bc44288552f
Plamen Dimitrov <plamendimitrov0308@yahoo.com>
2024-03-12 19:31:57 +0200

    fix allowed hosts

--- dental/settings.py
+++ dental/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0x1xrbn^$5rxedoq+bsxbj^qog%mkk8_vi8myo8ueyjxf$io0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
